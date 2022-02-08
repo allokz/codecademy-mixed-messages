bd6a56e92cf3edb5646b191a60e012d8fb21b4c2
Alexander Schenk <84243644+yodur@users.noreply.github.com>
2022-02-08 18:41:03 +0100

    Create mixed-messages.js file and add sentence blocks

---
+++ mixed-messages.js
@@ -0,0 +1,4 @@
+const subjects = [Ich, Du, Er, Sie, Es, Wir];
+const verbs = [essen, geben, finden, braten, zeichnen, reparieren, mixen];
+const adjectives = [leckere, langsame, schnelle, helle, dunkle, grüne, rote, gelbe, moderne, antike];
+const objects = [Freunde, Kuchen, Autos, Computer, Lampen, Glasflaschen, Telefone, Werkzeuge, Bilder];
