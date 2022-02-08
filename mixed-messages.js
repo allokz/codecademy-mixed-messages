2f82eeb3ad7bab0de71b4c542bb5c20c954527f3
Alexander Schenk <84243644+yodur@users.noreply.github.com>
2022-02-08 18:50:57 +0100

    Fix array entries to type string and add present tense form of verbs

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,4 +1,7 @@
-const subjects = [Ich, Du, Er, Sie, Es, Wir];
-const verbs = [essen, geben, finden, braten, zeichnen, reparieren, mixen];
-const adjectives = [leckere, langsame, schnelle, helle, dunkle, grüne, rote, gelbe, moderne, antike];
-const objects = [Freunde, Kuchen, Autos, Computer, Lampen, Glasflaschen, Telefone, Werkzeuge, Bilder];
+const subjects = ['Ich', 'Du', 'Er', 'Sie', 'Es', 'Wir'];
+const verbs = ['essen', 'geben', 'finden', 'braten', 'zeichnen', 'reparieren', 'mixen'];
+const verbsPresent = ['isst', 'gibt', 'findet', 'brät', 'zeichnet', 'repariert', 'mixt'];
+const adjectives = ['leckere', 'langsame', 'schnelle', 'helle', 'dunkle', 'grüne', 'rote', 'gelbe', 'moderne', 'antike'];
+const objects = ['Freunde', 'Kuchen', 'Autos', 'Computer', 'Lampen', 'Glasflaschen', 'Telefone', 'Werkzeuge', 'Bilder'];
+
+const randomSubject = subjects[Math.floor(Math.random() * subjects.length)];
